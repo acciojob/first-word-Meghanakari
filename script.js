71088d1a789e0f8317f6a779bd507cc6000d6ab3
Kari Meghana <karimeghana02@gmail.com>
2025-07-15 21:20:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 function firstWord(s) {
-  // your code here
-    let spaceIndex = s.indexOf(' ');
-    if (spaceIndex === -1) {
-        return s.trim();
-    }
-    return s.substring(0, spaceIndex).trim(); 
-	
-}
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
-
-function firstWord(s) {
 	let trimStr=s.trim();
 	let words=trimStr.split(" ");
 	return words[0];
